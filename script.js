79b3309a6a0e6a3ddc5711e2e40981a6dd464342
Drew Speed <drewjspeed@gmail.com>
2022-01-22 01:00:12 -0600

    added colorization perm time to userInput

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ $("#currentDay").text($(this).text().replace("", today));
 
 //loop to create separate rows for each hour
 for (var i=8; i<=17; i++) {
+
+    // add form rows for columns to sit in
+    formRow = $('<form class="row">');
+    endForm = $('</form>')
+
+    $(".container").append(formRow);
+    
+ 
+
     //create display hour with added meridian
     meridian = ""
     if (i < 12) {
@@ -22,20 +31,32 @@ for (var i=8; i<=17; i++) {
 
     displayHour = hour + meridian
 
-    // add form rows for columns to sit in
-    formRow = $('<form class="row">');
-    $(".container").append(formRow);
-
-    // add Time of day column to each row
+    // add field for displayHour to sit
     hourField = $("<div class='col-1 hour'><p class='time-block'>" + displayHour + "</p></div>");
    
 
+
     //add input textarea to each row
-    inputField = $("<div class='col-10 description'><textarea id='input" + i + "'> </textarea></div>");
-    
+    inputField = $("<div class='col-10'><textarea class='userInput' name='userInput'> </textarea></div>");
+
+
+    currentTime = moment().format("HH");
+
+    //add save field
    saveField = $("<button class='col-1 saveBtn' id='save" + i + "'><i class='far fa-save fa-lg'></i></button>");
 
-      $(formRow).append(hourField, inputField, saveField);
+   // append above additions to the page
+      $(formRow).append(hourField, inputField, saveField, endForm);
      
 
     }
+
+// colorize each row per time of day
+
+    if (i < currentTime) {
+        $(".userInput").addClass("past");
+    } else if (i == currentTime) {
+        $(".userInput").addClass("present");
+    } else {
+        $(".userInput").addClass("future");
+    };
